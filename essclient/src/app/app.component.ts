7d99387f11970cb7accc456598291cd0cadfe055
Radek Pilař <radek.pilar@disruptivelab.io>
2019-11-24 17:42:13 +0100

    add kentico service, connect to kentico project, create kentico account

--- essclient/src/app/app.component.ts
+++ essclient/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { LoaderService } from './services/loader.service';
 import { MDBSpinningPreloader } from 'ng-uikit-pro-standard';
 import { Component } from '@angular/core';
 import { TranslateMyService } from './services/translate-my.service';
+import { KenticoService } from './services/Kontent/kentico.service';
 
 @Component({
   selector: 'app-root',
@@ -15,12 +16,15 @@ export class AppComponent {
   constructor (
       private _translateMy: TranslateMyService,
       private _mdbSpinningPreloader: MDBSpinningPreloader,
-      private _loaderSrv: LoaderService
+      private _loaderSrv: LoaderService,
+      private _kenticoSrv: KenticoService
       ) {
     this._mdbSpinningPreloader.stop();
     this._translateMy.configTranslator();
 
     this._loaderSrv.loaderEmmitter
       .subscribe((show: boolean) => this.showLoader = show);
+
+    this._kenticoSrv.getItem('cs', 'home_page').then(x => console.log(x));
   }
 }
