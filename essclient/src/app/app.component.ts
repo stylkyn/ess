b289a3a0af4e5336427f961f9c603e040bc78d60
Radek Pilař <radek.pilar@disruptivelab.io>
2020-01-19 19:30:51 +0100

    remove kentico

--- essclient/src/app/app.component.ts
+++ essclient/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { LoaderService } from './services/loader.service';
 import { MDBSpinningPreloader } from 'ng-uikit-pro-standard';
 import { Component } from '@angular/core';
-import { KenticoService } from './services/Kontent/kentico.service';
 
 @Component({
   selector: 'app-root',
@@ -15,12 +14,9 @@ export class AppComponent {
   constructor (
       private _mdbSpinningPreloader: MDBSpinningPreloader,
       private _loaderSrv: LoaderService,
-      private _kenticoSrv: KenticoService
       ) {
     this._mdbSpinningPreloader.stop();
     this._loaderSrv.loaderEmmitter
       .subscribe((show: boolean) => this.showLoader = show);
-
-    this._kenticoSrv.getItem('cs', 'home_page').then(x => console.log(x));
   }
 }
