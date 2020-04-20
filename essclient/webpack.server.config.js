ef317c339c3d0c90b4fe460ff4e10a1049ed927f
Radek Pilar <radekpilar96@centrum.cz>
2020-04-20 20:57:12 +0200

    try fix deploy ssr

--- essclient/webpack.server.config.js
+++ essclient/webpack.server.config.js
@@ -10,7 +10,7 @@ module.exports = {
     server: './server.ts'
   },
   externals: {
-    './server/main': 'require("./server/main")'
+    './dist/server/main': 'require("./server/main")'
   },
   target: 'node',
   resolve: { extensions: ['.ts', '.js'] },
@@ -19,7 +19,7 @@ module.exports = {
   },
   output: {
     // Puts the output at the root of the dist folder
-    path: path.join(__dirname),
+    path: path.join(__dirname, 'dist'),
     filename: '[name].js'
   },
   module: {
