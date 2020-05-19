4f60f260f85396b1133ae7a01d11806cc4602e72
Radek Pilar <radekpilar96@centrum.cz>
2020-05-19 21:15:04 +0200

    fix footer links and cards links

--- essclient/src/app/presentation/pages/homepage/homepage.component.ts
+++ essclient/src/app/presentation/pages/homepage/homepage.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { presentationProductFullRoute } from '../../theme/presentation-routes';
 
 @Component({
-  selector: 'app-homepage',
-  templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.scss']
+    selector: 'app-homepage',
+    templateUrl: './homepage.component.html',
+    styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
+    presentationProductFullRoute = presentationProductFullRoute;
 
-  constructor() { }
+    constructor () { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
--- essclient/src/app/presentation/theme/footer/footer.component.ts
+++ essclient/src/app/presentation/theme/footer/footer.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { presentationProductFullRoute, presentationMyAccountFullOrderRoute } from '../presentation-routes';
+import { UserService } from './../../../services/API/user.service';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+    presentationProductFullRoute = presentationProductFullRoute;
+    presentationMyAccountFullOrderRoute = presentationMyAccountFullOrderRoute;
+    
+    constructor (public _userService: UserService) { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
