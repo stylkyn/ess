315b508e6a2a2787fea851e369986d198cae7e86
Radek Pilar <radekpilar96@centrum.cz>
2020-05-21 15:24:49 +0200

    try fix login

--- essclient/src/app/internal/pages/login/login.module.ts
+++ essclient/src/app/internal/pages/login/login.module.ts
@@ -4,22 +4,19 @@ import { routing } from './login.routing';
 import { LoginComponent } from './login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginCardComponent } from './login-card/login-card.component';
-import { InputsModule, InputUtilitiesModule, WavesModule, ButtonsModule } from 'ng-uikit-pro-standard';
 import { RouterModule } from '@angular/router';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @NgModule({
     imports: [
         CommonModule,
         routing,
         FormsModule,
-        InputsModule,
-        InputUtilitiesModule,
-        WavesModule,
-        ButtonsModule,
         ReactiveFormsModule,
         NzInputModule,
+        NzButtonModule,
         NzFormModule,
     ],
     declarations: [
