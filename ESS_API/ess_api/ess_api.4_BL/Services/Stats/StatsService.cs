440a622a4ba478a15a23c02f564c312a74449187
Radek Pilar <radekpilar96@centrum.cz>
2021-01-10 15:22:04 +0100

    oprava stats, vzdy generovat 6 mesicu

--- ESS_API/ess_api/ess_api.4_BL/Services/Stats/StatsService.cs
+++ ESS_API/ess_api/ess_api.4_BL/Services/Stats/StatsService.cs
@@ -5,6 +5,7 @@ using ess_api.Core.Constant;
 using ess_api.Core.Model;
 using ess_api.Core.Model.Shared;
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
@@ -28,10 +29,38 @@ namespace ess_api._4_BL.Services.Stats
                     ProfitTotal = new Price(g.Sum(p => p.CalculatedData.Total.TotalPrice.CzkWithoutVat), VatTypes.Czk21, PriceTypes.Czk),
                     SalesCount = g.Count()
                 })
-                .OrderByDescending(x => x.YearNumber)
+                .OrderBy(x => x.YearNumber)
                 .ThenBy(x => x.MonthNumber)
                 .Take(12)
                 .ToList();
+            if (result.Profits.Count() < 6)
+            {
+                var missingMonths = new List<StatsProfit>();
+                int missingMonthsCount = 6 - result.Profits.Count();
+                for (int i = 1; i <= missingMonthsCount; i++)
+                {
+                    int lastMonth = result.Profits.FirstOrDefault()?.MonthNumber ?? new DateTime().Month;
+                    int lastYear = result.Profits.FirstOrDefault()?.YearNumber ?? new DateTime().Year;
+
+                    bool isPreviosYear = lastMonth - i <= 0;
+                    int month = isPreviosYear  ? 12 + (lastMonth - i + 1) : lastMonth - i;
+                    int year = result.Profits.FirstOrDefault()?.YearNumber ?? new DateTime().Year;
+                    if (isPreviosYear)
+                    {
+                        year -= 1;
+                    }
+
+                    missingMonths.Add(new StatsProfit
+                    {
+                        YearNumber = year,
+                        MonthNumber = month,
+                        MonthName = new DateTime(year, month, 1).ToString("MMMM", CultureInfo.GetCultureInfo("cs")),
+                        ProfitTotal = new Price(0, VatTypes.Czk21, PriceTypes.Czk),
+                        SalesCount = 0
+                    });
+                }
+                result.Profits = missingMonths.Concat(result.Profits).ToList();
+            }
             return new Response<StatsResponse>(ResponseStatus.Ok, _mapService.MapStats(result));
         }
 
